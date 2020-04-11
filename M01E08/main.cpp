8e20b553cdf94871c972bc4d978f46c0a68628cd
deadwingg <ivgomez505@gmail.com>
2020-04-11 12:43:39 -0300

    version inicial

--- M01E08/main.cpp
+++ M01E08/main.cpp
@@ -6,5 +6,21 @@ int main() {
     int numero = 0;
     cout << "Ingrese numero entero de 5 digitos";
     cin >> numero;
+    int fifth = numero % 10;
+    numero /= 10;
+    int fourth = numero % 10;
+    numero /= 10;
+    int third = numero % 10;
+    numero /= 10;
+    int second = numero % 10;
+    numero /= 10;
+    int first = numero;
+    
+    cout << "Cifras del numero: " << endl;
+    cout << first << endl;
+    cout << second << endl;
+    cout << third << endl;
+    cout << fourth << endl;
+    cout << fifth << endl; 
     return 0;
 }
