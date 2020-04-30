8d096c004c906fbab72e22a6f9ad7ad5f1448540
deadwingg <51037472+deadwingg@users.noreply.github.com>
2020-04-30 11:40:18 -0300

    Principio del ejercicio 20

---
+++ TrabajoPractico1/ej20/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Se dispone de un lote de valores enteros positivos que finaliza con un número negativo.
+ * El lote está dividido en sublotes por medio de valores cero.
+ * Desarrollar un programa que determine e informe:
+ *  a) por cada sublote el promedio de valores
+ *  b) el total de sublotes procesados
+ *  c) el valor máximo del conjunto, indicando en que sublote se encontró
+ *  y la posición relativa del mismo dentro del sublote
+ *  d) valor mínimo de cada sublote
+ * Nota: el lote puede estar vacío (primer valor negativo), o puede haber uno, varios o todos los
+ * sublotes vacíos (ceros consecutivos)
+ */
+#include <iostream>
+using namespace std;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
