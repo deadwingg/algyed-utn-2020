073cdb6cfdf4f8abdee1aa5e7bd937b24a39ddd3
deadwingg <51037472+deadwingg@users.noreply.github.com>
2020-05-21 10:45:58 -0300

    Corregido el informe de lus numeros ingresados.

--- TrabajoPractico1/ej13/main.cpp
+++ TrabajoPractico1/ej13/main.cpp
@@ -2,17 +2,21 @@
  *  Ingresar e informar valores, mientras que el valor ingresado no sea negativo.
  *  Informar la cantidad de valores ingresados
  *
- *  en la cuenta contamos el valor negativo como ingresado
+ *  No contamos el -1 como numero ingresado
  */
 #include <iostream>
 
 using namespace std;
 int main() {
-    cout << "Ingrese numeros, dato negativo para salir.";
+    cout << "Ingrese numeros, dato negativo para salir.\n";
     int cuenta = 0;
     int numero = 1;
     while (numero >= 0){
         cin >> numero;
+        if (numero < 0){
+            break;
+        }
+        cout << numero;
         cuenta++;
     }
     cout << "Se han ingresado " << cuenta << "numeros.\n";
